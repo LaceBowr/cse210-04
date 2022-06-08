4d82e6f4ceb9775796d90e235c42b237e10031bf
Webdeveloperintraining <103979055+Webdeveloperintraining@users.noreply.github.com>
2022-06-07 18:02:33 -0700

    now it returns how many spaces is moving

--- keyboard-class.py
+++ keyboard-class.py
@@ -6,30 +6,35 @@ class Keyboard_service:
     
     The responsibility of the Keyboard_services class is to detect player key presses and translate them into 
     a point representing a direction.
-
     Attributes:
         cell_size (int): For scaling directional input to a grid.
     """    
     def __init__(self, cell_size=1):
         self._cell_size=cell_size
-    
-    def define_direction(self):
+        self.dx=0
+        self.dy=0
+ def get_direction(self):
+        #takes input done by character in play and implements a move
         """Gets the selected direction based on the currently pressed keys.
 
         Returns:
             Point: The selected direction.
         """
-        x_movement=0
-        dy=0
-
         if pyray.is_key_down(pyray.KEY_LEFT):
-            x_movement=-1
+            dx = -1
+        
         if pyray.is_key_down(pyray.KEY_RIGHT):
-            x_movement=1
-    #Class that defines the horizontal distance from a certain point in the display
-        direction: Point(x_movement,dy) 
-        direction=direction.scale(self._cell_size)
-    
-        return direction
+            dx = 1
+
+        dx,dy = self.new_direction()# = (dx,dy)
+        #self.new_direction == get_direction(dx,dy) 
+  
+        return dx,dy
+
+    def new_direction(self):
+        #records the new changes made
+        move = self.dx * pyray.is_key_down(pyray.KEY_LEFT) or self.dx * pyray.is_key_down(pyray.KEY_RIGHT)
+        self.dx = 30 + move
+        return self.dx,self.dy
 
 
