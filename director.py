9f8feb0e00f1ee94ebabfca11a8b885347c4a7a0
Fireguy13 <104537058+Fireguy13@users.noreply.github.com>
2022-06-08 18:09:27 -0600

    update

--- director.py
+++ director.py
@@ -1,5 +1,6 @@
 #from character import Character
 #from keyboard_services import KeyboardService
+from keyboard_services import KeyboardService
 from video_service import VideoService
 from symbols import Symbols
 from time import sleep
@@ -17,21 +18,24 @@ class Director():
         self.get_character_score += self.update_score.append()
         return self.update_score
 
-    def start_game(self):
-        vs = VideoService()
-        vs.open_window()
-        syms = Symbols()
+    def start_game(self, new_direction):
+        
+        question = input('Experience the thrill having gems or rocks fall upon you! <i>Greed</i>\nmight seem easy, but even the best treasure-hunter can end up in the wrong place and \nget hit by rocks.\nThe rules are simple.\n The character can move\non the screen trying to dodge or hit the falling objects.\n*If your character(#) move connects with a gem(*) you get a +1 to your score.\n*If the character move connects with a rock you get a -1 to your score.\n\nDo you want to start playing? y/n: ')   
+        if question =="y":
+            vs = VideoService()
+            vs.open_window()
+            syms = Symbols()
+        else:
+            exit()
         
         # Demonstrate functional video service
         vs.draw_grid(syms)
         sleep(10)
         
-        # while loop while game is running
-            # get keyboard direction
-            # update positions of all symbols
-            # update score
-            # redraw the grid
-
-        
-
-    
+        while self.get_direction == True:
+            VideoService.draw_grid()
+            Symbols(KeyboardService.dx,KeyboardService.dy,(0,0,255,100),'#', False, player=True)
+            Symbols.symbols.append()
+            Director.update_score = print(f'{self.update_score}')
+            
+            return VideoService
--- gems_rocks.py
+++ gems_rocks.py
@@ -1,80 +1,67 @@
 import random
 
 colors = ["Red","Grey","Blue","White","Green"]
-column = (0,1,2,3,4,5,6,7,8,9)
-
-
+column = (0,39)
 
 class NewItem:
-    def __init__(self):        
-        self.col = random.Random().choice(colors)
+   def __init__(self):       
+       self.col = random.Random().choice(colors)
 
-
-    def get_CaptureItem(self):
-        if self.col == "Red" or self.col == "Blue" or self.col == "Green":
-            ci = Gem(random.choice(column))
-            ci.clr = self.col
-        else: 
-            ci = Rock(random.choice(column))  
-            ci.clr = self.col     
-        return ci
-        
-       
+   def get_CaptureItem(self):
+       if self.col == "Red" :
+           ci = Gem(random.choice(column))
+           ci.clr = self.col
+       else:
+           ci = Rock(random.choice(column)) 
+           ci.clr = self.col    
+       return ci
 
 class CaptureItem:
-    def __init__(self,position,color):
-        self.pos = position
-        self.clr = color
-    
-    
+   def __init__(self,position,color):
+       self.pos = position
+       self.clr = color
 
-class Gem(CaptureItem):
-    def __init__ (self,position):
-        self.score = 0
-        self.pos = position
+class Gem_rock(CaptureItem):
+   def __init__ (self,position):
+       self.score = 0
+       self.pos = position
 
-    def get_score(self):
-        if self.clr == "Red":
-            return 5
-        elif self.clr == "Blue":
-            return 3
-        else:
-            return 1
 
-    def is_hit(self):
-        return ScoreResult(self.get_score(), self)
-       
+   def get_score(self):
+       if self.clr == "Red":
+           return +1
+       else:
+           return -1
+
+   def is_hit(self):
+       return ScoreResult(self.get_score(), self)
 
 class Rock(CaptureItem):
-    def __init__ (self,position):
-        self.score = 0
-        self.pos = position
+   def __init__ (self,position):
+       self.score = 0
+       self.pos = position
 
-    
+   def get_score(self):
+       if self.clr == "White":
+           return -1
+       else:
+           return +1
 
-    def get_score(self):
-        if self.clr == "White":
-            return -2
-        else:
-            return -1
-    
-    def is_hit(self):
-        return ScoreResult(self.get_score(), self)
+   def is_hit(self):
+       return ScoreResult(self.get_score(), self)
 
 class ScoreResult:
-    def __init__(self,score,captured):
-        self.sc = score
-        self.ci = captured
-
+   def __init__(self,score,captured):
+       self.sc = score
+       self.ci = captured
 
 def main():
-    newItm = NewItem()
-    capturedItm = newItm.get_CaptureItem()
-    result = capturedItm.is_hit()
-    print("Here are the results:")
-    print(f"Score:""{result.sc}")
-    print(result.ci.clr)
-    
+   newItm = NewItem()
+   capturedItm = newItm.get_CaptureItem()
+   result = capturedItm.is_hit()
+   print("Here are the results:")
+   print(f"Score:""{result.sc}")
+   print(result.ci.clr)
 
 if __name__== "__main__":
-    main()
+   main() 
